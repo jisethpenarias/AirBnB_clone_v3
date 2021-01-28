e716c59bdd18fe9e982f7c3294edb4545d6a25f3
jpenariasholberton <1875@holbertonschool.com>
2021-01-27 21:36:52 -0500

    adding mwthod stats: number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status_route():
     """ Returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def stats_route():
+    """ Retrieves the number of each objects """
+    number_objects = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+        }
+    return jsonify(number_objects)
